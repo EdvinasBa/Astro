9a3f7410fb292ea120eab6c14dc75723e72f1bef
edvinasba <edvinas.balciunass@gmail.com>
2022-08-31 17:06:34 +0300

    RM mouse animation 60fps limit

--- src/composables/useMousePosition.ts
+++ src/composables/useMousePosition.ts
@@ -6,28 +6,12 @@ import {
 
 import type { Ref } from 'vue';
 
-export const MOUSE_ANIMATION_REFRESH_RATE: number = 1000 / 60;
-
 export const useMousePosition = () => {
 	const mouseX: Ref<number> = ref(null);
 	const mouseY: Ref<number> = ref(null);
 	const rawMouseEvent: Ref<MouseEvent> = ref(null);
 
-	let prevTimeStamp: number = null;
-
-	const handleMoveMouse = (e: MouseEvent) => requestAnimationFrame((timeStamp) => {
-		if (prevTimeStamp === timeStamp) {
-			return;
-		}
-
-		if (!prevTimeStamp) {
-			prevTimeStamp = timeStamp;
-		}
-
-		if (timeStamp - prevTimeStamp < MOUSE_ANIMATION_REFRESH_RATE) {
-			return;
-		}
-
+	const handleMoveMouse = (e: MouseEvent) => requestAnimationFrame(() => {
 		mouseX.value = e.clientX;
 		mouseY.value = e.clientY + window.scrollY;
 
