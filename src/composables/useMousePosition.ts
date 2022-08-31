69e2dfb6ee5faab34a38ba02e957d97acf6f50a5
edvinasba <edvinas.balciunass@gmail.com>
2022-08-31 14:31:27 +0300

    use requestAnimationFrame in mouse animations to make things smooth on high refresh screens

--- src/composables/useMousePosition.ts
+++ src/composables/useMousePosition.ts
@@ -6,17 +6,33 @@ import {
 
 import type { Ref } from 'vue';
 
+export const MOUSE_ANIMATION_REFRESH_RATE: number = 1000 / 60;
+
 export const useMousePosition = () => {
 	const mouseX: Ref<number> = ref(null);
 	const mouseY: Ref<number> = ref(null);
 	const rawMouseEvent: Ref<MouseEvent> = ref(null);
 
-	const handleMoveMouse = (e) => {
+	let prevTimeStamp: number = null;
+
+	const handleMoveMouse = (e: MouseEvent) => requestAnimationFrame((timeStamp) => {
+		if (prevTimeStamp === timeStamp) {
+			return;
+		}
+
+		if (!prevTimeStamp) {
+			prevTimeStamp = timeStamp;
+		}
+
+		if (timeStamp - prevTimeStamp < MOUSE_ANIMATION_REFRESH_RATE) {
+			return;
+		}
+
 		mouseX.value = e.clientX;
 		mouseY.value = e.clientY + window.scrollY;
 
 		rawMouseEvent.value = e;
-	};
+	});
 
 	onMounted(() => {
 		document.addEventListener('mousemove', handleMoveMouse);
